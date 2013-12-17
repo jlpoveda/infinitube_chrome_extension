b2fd4d8789fe608102fc29964e1a93dd052ddac5
Jose Luis Poveda <joseluis.poveda@ofertix.com>
2013-12-17 15:49:57 +0100

    Empezando

--- myscript.js
+++ myscript.js
@@ -1,9 +1,22 @@
 // Checking page title
-if (document.title.indexOf("Google") != -1) {
-    //Creating Elements
-    var btn = document.createElement("BUTTON")
-    var t = document.createTextNode("CLICK ME");
+if (document.title.indexOf("YouTube") != -1) {
+    var $span = document.getElementById("watch7-sentiment-actions");
+    var btn=document.createElement("BUTTON");
+    var t=document.createTextNode("View on Infinitube");
     btn.appendChild(t);
-    //Appending to DOM 
-    document.body.appendChild(btn);
+    $span.appendChild(btn);
+
+
+    var $span = document.getElementById("watch7-sentiment-actions");
+    var btn=document.createElement("BUTTON");
+    var t=document.createTextNode("View on Infinitube");
+    btn.appendChild(t);
+    var s = document.createElement('SPAN');
+    s.className='yt-uix-button-icon-wrapper';
+    btn.appendChild(s);
+    $span.appendChild(btn);
 }
+
+<span class="yt-uix-button-icon-wrapper">
+    <img class="yt-uix-button-icon yt-uix-button-icon-watch-dislike" src="http://s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" alt="No me gusta este vídeo" title="">
+</span>

b35740bed8a5cf48fcb99561b6a563acdd329ceb
Jose Luis Poveda <jlpoveda@gmail.com>
2013-12-17 18:08:30 +0100

    Script casi finalizado

--- myscript.js
+++ myscript.js
@@ -1,11 +1,38 @@
-// Checking page title
-if (document.title.indexOf("YouTube") != -1) {
-    var objDiv = document.getElementById("eow-title");
-    var newImage = document.createElement("img");
-    newImage.src = "http://beta.infinitube.com/img/infinitube-mini.png";
-    newImage.onclick = function(){
-        window.location = 'http://beta.infinitube.com?v='+ytplayer.config.args.video_id;
+function getVideoid() {
+    var links = document.getElementsByTagName('link');
+    for (i=0; i<links.length; i++) {
+        if (links[i].getAttribute("rel") == "shortlink") {
+            return links[i].getAttribute("href").replace('http://youtu.be/', '');
+        }
     }
-    newImage.style.cursor='pointer';
-    objDiv.insertBefore(newImage,objDiv.firstChild);
+    return false;
 }
+function loadImg() {
+    var videoId = getVideoid();
+    if (false != videoId && document.title.indexOf("YouTube") != -1) {
+        var objDiv = document.getElementById("eow-title");
+        var newImage = document.createElement("img");
+        newImage.src = "http://beta.infinitube.com/img/infinitube-mini.png";
+        newImage.onclick = function(){
+            window.open(
+                'http://beta.infinitube.com?v='+videoId,
+                '_blank'
+            );
+        }
+        newImage.style.cursor='pointer';
+        newImage.style.height='18px';
+        objDiv.insertBefore(newImage,objDiv.firstChild);
+        objDiv.onchange=function(){loadImg()};
+    }
+}
+
+window.onload = function() {
+    loadImg();
+}
+var storedUrl = window.location.href;
+window.setInterval(function () {
+    if (window.location.href != storedUrl) {
+        storedUrl = window.location.href;
+        loadImg();
+    }
+}, 4000);

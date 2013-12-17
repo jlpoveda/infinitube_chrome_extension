cc39b34d0aa48cdf508c10552fd3f71d34ebc39d
Jose Luis Poveda <jlpoveda@gmail.com>
2013-12-17 16:29:03 +0100

    Código funcional

--- myscript.js
+++ myscript.js
@@ -1,22 +1,11 @@
 // Checking page title
 if (document.title.indexOf("YouTube") != -1) {
-    var $span = document.getElementById("watch7-sentiment-actions");
-    var btn=document.createElement("BUTTON");
-    var t=document.createTextNode("View on Infinitube");
-    btn.appendChild(t);
-    $span.appendChild(btn);
-
-
-    var $span = document.getElementById("watch7-sentiment-actions");
-    var btn=document.createElement("BUTTON");
-    var t=document.createTextNode("View on Infinitube");
-    btn.appendChild(t);
-    var s = document.createElement('SPAN');
-    s.className='yt-uix-button-icon-wrapper';
-    btn.appendChild(s);
-    $span.appendChild(btn);
+    var objDiv = document.getElementById("eow-title");
+    var newImage = document.createElement("img");
+    newImage.src = "http://beta.infinitube.com/img/infinitube-mini.png";
+    newImage.onclick = function(){
+        window.location = 'http://beta.infinitube.com?v='+ytplayer.config.args.video_id;
+    }
+    newImage.style.cursor='pointer';
+    objDiv.insertBefore(newImage,objDiv.firstChild);
 }
-
-<span class="yt-uix-button-icon-wrapper">
-    <img class="yt-uix-button-icon yt-uix-button-icon-watch-dislike" src="http://s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" alt="No me gusta este vídeo" title="">
-</span>
